543016b1dd104e8eebf4803befe0e20c6c645c98
André Moreira <andre.moreira.9@hotmail.com>
2021-12-16 16:15:10 +0000

    Add safeguard if query fails

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 use App\Models\Product;
+use Exception;
 
 class ProductController extends Controller {
 
@@ -22,57 +23,64 @@ class ProductController extends Controller {
     }
 
     public function list(Request $request) {
-        $query = DB::table('product')
-            ->whereRaw('stock > 0')
-            ->when($request->text, function ($q) use ($request) {
-                return $q->whereRaw('tsvectors @@ plainto_tsquery(\'english\', ?)', [$request->text])
-                    ->orderByRaw('ts_rank(tsvectors, plainto_tsquery(\'english\', ?)) DESC', [$request->text]);
-            })
-            ->when($request->input('price-min'), function ($q) use ($request) {
-                return $q->where('price', '>', [$request->input('price-min')]);
-            })
-            ->when($request->input('price-max'), function ($q) use ($request) {
-                return $q->where('price', '<', [$request->input('price-max')]);
-            })
-            ->when($request->input('rate-min'), function ($q) use ($request) {
-                return $q->where('avg_stars', '>', [$request->input('rate-min')]);
-            })
-            ->when($request->input('rate-max'), function ($q) use ($request) {
-                return $q->where('avg_stars', '<', [$request->input('rate-max')]);
-            });
+        try {
+            $query = DB::table('product')
+                ->whereRaw('stock > 0')
+                ->when($request->text, function ($q) use ($request) {
+                    return $q->whereRaw('tsvectors @@ plainto_tsquery(\'english\', ?)', [$request->text])
+                        ->orderByRaw('ts_rank(tsvectors, plainto_tsquery(\'english\', ?)) DESC', [$request->text]);
+                })
+                ->when($request->input('price-min'), function ($q) use ($request) {
+                    return $q->where('price', '>', [$request->input('price-min')]);
+                })
+                ->when($request->input('price-max'), function ($q) use ($request) {
+                    return $q->where('price', '<', [$request->input('price-max')]);
+                })
+                ->when($request->input('rate-min'), function ($q) use ($request) {
+                    return $q->where('avg_stars', '>', [$request->input('rate-min')]);
+                })
+                ->when($request->input('rate-max'), function ($q) use ($request) {
+                    return $q->where('avg_stars', '<', [$request->input('rate-max')]);
+                });
 
-        switch ($request->order) {
-            case 'price-asc':
-                $query = $query->orderBy('price');
-                break;
-            case 'price-desc':
-                $query = $query->orderByDesc('price');
-                break;
-            case 'rate-asc':
-                $query = $query->orderBy('avg_stars');
-                break;
-            case 'rate-desc':
-                $query = $query->orderByDesc('avg_stars');
-                break;
+            switch ($request->order) {
+                case 'price-asc':
+                    $query = $query->orderBy('price');
+                    break;
+                case 'price-desc':
+                    $query = $query->orderByDesc('price');
+                    break;
+                case 'rate-asc':
+                    $query = $query->orderBy('avg_stars');
+                    break;
+                case 'rate-desc':
+                    $query = $query->orderByDesc('avg_stars');
+                    break;
+            }
+
+            $pageSize = $request->input('page-size');
+            $page = $request->page;
+
+            $count = $query->count();
+
+            $lastPage = ceil($count / $pageSize);
+
+            if ($request->page > $lastPage) {
+                $page = $lastPage - 1;
+            }
+
+            $query = $query->skip($page * $pageSize)->take($pageSize);
+
+            return response()->json([
+                "lastPage" => $lastPage,
+                "docCount" => $count,
+                "query" => $query->get()
+            ]);
+        } catch (Exception) {
+            return response()->json(
+                ['message' => 'Unexpected error'],
+                401
+            );
         }
-
-        $pageSize = $request->input('page-size');
-        $page = $request->page;
-
-        $count = $query->count();
-
-        $lastPage = ceil($count / $pageSize);
-
-        if ($request->page > $lastPage) {
-            $page = $lastPage - 1;
-        }
-
-        $query = $query->skip($page * $pageSize)->take($pageSize);
-
-        return response()->json([
-            "lastPage" => $lastPage,
-            "docCount" => $count,
-            "query" => $query->get()
-        ]);
     }
 }
