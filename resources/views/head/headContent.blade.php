d98ff7a83885d1eca761c35dddec6772952df559
biromiro <up201906272@up.pt>
2021-12-27 11:45:06 +0000

    Fix page display

--- resources/views/head/headContent.blade.php
+++ resources/views/head/headContent.blade.php
@@ -11,6 +11,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Serif+Text:ital@0;1&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/product.css') }}" rel="stylesheet">
 
     <!-- Styles -->
     {{-- <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet"> --}}
