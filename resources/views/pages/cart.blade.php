5541fbbdd9b6434ed111ca763f11d83f3c6d6b0b
biromiro <up201906272@up.pt>
2021-12-27 13:08:31 +0000

    Fixes on cart view, add default when cart is empty

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -4,43 +4,59 @@
 
 @section('content')
 
+
 <section id="cart">
-    <h3 class="mx-2">{{$user->name}}'s Cart</h3>
-    <div class="d-flex">
-        <div class="w-50 mx-4">
-            @each('partials.cartproduct', $cart , 'cart_item')
+    @if($cart->empty())
+        <div class="d-flex justify-content-center align-items-center flex-column"> 
+            <h3>You have no items in your cart. Go get some...</h3>
+            <form action="/products/">
+                <button type="submit" class="btn btn-primary">Go to Product List</button>
+            </form>
         </div>
-        <div class="mx-4">
-            <div class="my-4">
-                <h4>Apply Coupon </h4>
-                <div class="d-flex">
-                    <input type="text">
-                    <button type="button" class="btn btn-primary mx-4">Apply Coupon</button>
+    @else
+    <h3 class="mx-2 text-center">{{$user->name}}'s Cart</h3>
+    <div class="container-fluid px-5">
+        <div class="row">
+            <div class="col-8">
+                @each('partials.cartproduct', $cart , 'cart_item')
+            </div>
+            <div class="col-4 d-flex align-items-center justify-content-center flex-column">
+                <div class="my-4 w-75">
+                    <h4>Apply Coupon </h4>
+                    <div class="d-flex my-3 align-items-center">
+                        <input class="w-50" type="text">
+                        <button type="button" class="btn btn-primary mx-3 p-1 w-25">Apply</button>
+                    </div>
                 </div>
-            </div>
-            <div class="my-4">
-                <h4>Order Summary</h4>
-                <br>
-                <table id="vertical-1" style="border-spacing: 2em .5em; border-collapse: inherit;">
-                    <tr>
-                        <th>Subtotal (Tax Excluded):</th>
-                        <td>124.99 €</td>
-                    </tr>
-                    <tr>
-                        <th>Total Tax (IVA):</th>
-                        <td>26.25 €</td>
-                    </tr>
-                    <tr style="font-size: 1.3em;">
-                        <th>Total (Tax Included):</th>
-                        <td>151.24 €</td>
-                    </tr>
-                </table>
-            </div>
-            <div class="my-4">
-                <button type="button" class="btn btn-primary">Proceed to Purchase Process</button>
+                <div class="my-4 container">
+                    <div class="row mx-3">
+                        <h4>Order Summary</h4>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <table id="vertical-1" style="border-spacing: 2em .5em; border-collapse: inherit;">
+                            <tr>
+                                <th>Subtotal (Tax Excluded):</th>
+                                <td>124.99 €</td>
+                            </tr>
+                            <tr>
+                                <th>Total Tax (IVA):</th>
+                                <td>26.25 €</td>
+                            </tr>
+                            <tr style="font-size: 1.3em;">
+                                <th>Total (Tax Included):</th>
+                                <td>151.24 €</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="my-4 w-50 d-flex align-items-center justify-content-center">
+                    <button type="button" class="btn btn-primary w-100">Proceed to Checkout</button>
+                </div>
             </div>
         </div>
     </div>
+    @endif
 </section>
 
 @endsection
--- resources/views/partials/cartproduct.blade.php
+++ resources/views/partials/cartproduct.blade.php
@@ -1,25 +1,27 @@
 <div class="card my-3" style="height: 8em;">
     <div class="row g-6 h-100">
       <div class="col-md-2 h-100">
-        <img src={{Storage::url('images/product/default.jpg')}} class="img-fluid rounded-start h-100" style="object-fit: cover;">
+        <img src="/img/default.jpg" class="img-fluid rounded-start h-100" style="object-fit: cover;">
       </div>
       <div class="col">
-        <div class="card-body d-flex justify-content-between w-100">
-          <div>
-            <h5 class="card-title">{{$cart_item->name}}</h5>
-            <p class="card-text">{{$cart_item->description}}</p>
-            <p class="card-text"><small class="text-muted">
-                    @for ($i = 1; $i <= 5; $i++)
-                        <i class="bi bi-star{{floor($cart_item->avg_stars) >= $i ? '-fill' : (ceil($cart_item->avg_stars) == $i ? '-half' : '')}}"></i>
-                    @endfor</small></p>
-          </div>
-          <div>
-            <h5> Amount </h5>
-            <p>{{$cart_item->details->amount}}</p>
-          </div>
-          <div>
-            <h5>Price</h5>
-            <p> {{$cart_item->price}} €</p>
+        <div class="card-body container-fluid">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title">{{$cart_item->name}}</h5>
+              <p class="card-text">{{$cart_item->description}}</p>
+              <p class="card-text"><small class="text-muted">
+                      @for ($i = 1; $i <= 5; $i++)
+                          <i class="bi bi-star{{floor($cart_item->avg_stars) >= $i ? '-fill' : (ceil($cart_item->avg_stars) == $i ? '-half' : '')}}"></i>
+                      @endfor</small></p>
+            </div>
+            <div class="col">
+              <h5> Amount </h5>
+              <p>{{$cart_item->details->amount}}</p>
+            </div>
+            <div class="col">
+              <h5>Price</h5>
+              <p> {{$cart_item->price}} €</p>
+            </div>
           </div>
         </div>
       </div>
