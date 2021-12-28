98bea36a1e029fd2fbe06f23cdb799792bef7e00
Nuno Alves <up201908250@fe.up.pt>
2021-12-28 14:25:33 +0000

    add: form to post checkout

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div class="container">
-    <div class="row">
+    <form class="row" method="POST" action="">
         <div class="col-md-8">
             <section id="order-address" class="mb-4">
                 <h2 class="mb-4">Address</h2>
@@ -36,9 +36,9 @@
                                 <div class="col-md-4 d-flex justify-content-end align-items-end">
                                     <div class="form-check">
                                         <input class="form-check-input" onclick="styleChosenAddress({{$loop->index}})"
-                                            type="radio" name="address-radio" id="address-radio{{$loop->index}}"
-                                            @if($loop->first) checked @endif>
-                                        <label class="form-check-label" for="address-radio">
+                                            type="radio" name="address-id" value="{{$address->id}}"  id="address-radio{{$loop->index}}"
+                                            @if($loop->first) checked @endif required>
+                                        <label class="form-check-label" for="address-id">
                                             Use this address
                                         </label>
                                     </div>
@@ -51,33 +51,33 @@
             </section>
             <section id="order-payment" class="mb-4 d-flex flex-column">
                 <h2 class="mb-4">Payment Method</h2>
-                <div class="payment-option mb-4">
-                    <div class="row container justify-content-between">
-                        <div class="col-md-6 payment-image">
+                <div class="payment-option container mb-4">
+                    <div class="row justify-content-between">
+                        <div class="col-6 payment-image">
                             <img style="" src="{{asset("img/paypal.png")}}">
                         </div>
-                        <div class="col-md-4 d-flex justify-content-end align-items-center">
+                        <div class="col-4 d-flex justify-content-start align-items-center">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="payment-radio"
-                                    id="payment-radio-paypal">
-                                <label class="form-check-label" for="payment-radio">
+                                <input class="form-check-input" type="radio" name="payment-type"
+                                    id="payment-radio-paypal" value="paypal" required>
+                                <label class="form-check-label" for="payment-type">
                                     Pay with PayPal
                                 </label>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="payment-option mb-4">
-                    <div class="row container justify-content-between">
-                        <div class="col-md-6 payment-image">
-                            <img style="" src="{{asset("img/paypal.png")}}">
+                <div class="payment-option container mb-4">
+                    <div class="row justify-content-between">
+                        <div class="col-6 payment-image">
+                            {{-- <img style="" src="{{asset("img/paypal.png")}}"> --}}
                         </div>
-                        <div class="col-md-4 d-flex justify-content-end align-items-center">
+                        <div class="col-4 d-flex justify-content-start align-items-center">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="payment-radio"
-                                    id="payment-radio-paypal">
-                                <label class="form-check-label" for="payment-radio">
-                                    Pay with PayPal
+                                <input class="form-check-input" type="radio" name="payment-type"
+                                    id="payment-radio-bank" value="bank" required>
+                                <label class="form-check-label" for="payment-type">
+                                    Pay with Bank Transfer
                                 </label>
                             </div>
                         </div>
@@ -86,9 +86,9 @@
             </section>
         </div>
         <div class="col-md-4 d-flex align-items-center justify-content-start flex-column">
-            COUPONS AND STUFF HERE
+            <button type="submit" class="btn btn-primary">Checkout</button>
         </div>
-    </div>
+    </form>
 </div>
 
 <script defer>
