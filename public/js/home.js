fc14be4d9467bbf6df7f29b530be8a72f39703ea
biromiro <up201906272@up.pt>
2021-12-28 21:36:31 +0000

    Set responsiveness on mobile with a video

--- public/js/home.js
+++ public/js/home.js
@@ -6,7 +6,7 @@ var sentences = [
 ];
 
 let currentSentence = 0;
-let currentSentenceIndex = 0;
+let currentSentenceLetter = 0;
 let interval;
 
 // Element that holds the text
@@ -18,9 +18,9 @@ let cursor = document.querySelector("#cursor");
 // Implements typing effect
 function typeEffect() { 
 	// Get substring with 1 characater added
-	var text =  sentences[currentSentence].substring(0, currentSentenceIndex + 1);
+	var text =  sentences[currentSentence].substring(0, currentSentenceLetter + 1);
 	keywordContainer.innerHTML = text;
-	currentSentenceIndex++;
+	currentSentenceLetter++;
 
 	// If full sentence has been displayed then start to delete the sentence after some time
 	if(text === sentences[currentSentence]) {
@@ -37,21 +37,17 @@ function typeEffect() {
 // Implements deleting effect
 function deleteEffect() {
 	// Get substring with 1 characater deleted
-	var text =  sentences[currentSentence].substring(0, currentSentenceIndex - 1);
+	var text =  sentences[currentSentence].substring(0, currentSentenceLetter - 1);
 	keywordContainer.innerHTML = text;
-	currentSentenceIndex--;
+	currentSentenceLetter--;
 
 	// If sentence has been deleted then start to display the next sentence
 	if(text === '') {
 		clearInterval(interval);
 
 		// If current sentence was last then display the first one, else move to the next
-		if(currentSentence == (sentences.length - 1))
-            currentSentence = 0;
-		else
-            currentSentence++;
-		
-        currentSentenceIndex = 0;
+        currentSentence = (currentSentence + 1) % sentences.length		
+        currentSentenceLetter = 0;
 
 		// Start to display the next sentence after some time
 		setTimeout(function() {
@@ -69,7 +65,7 @@ const logoContainer = document.getElementById('logo-container');
 
 window.addEventListener("scroll", () => {
     var y = window.scrollY;
-    if (y >= 50){
+    if (y >= 350){
         logoContainer.classList.add('remove');
         return;
     }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,13 +5,13 @@
     <script type="text/javascript" src={{ asset('js/home.js') }} defer></script>
   <body>        
     <div class="video-container">
-      <!--<video autoplay poster="" class="video-parallax" loop muted>
-        <source src={{asset('img/homepage.mp4')}} type="video/mp4">
-      </video>-->
-      <img class="video-parallax" src="/img/homepage.jpg" alt=""/>
+      <video autoplay poster="" class="video-parallax video-mobile" loop muted>
+        <source src="img/homepage.mp4" type="video/mp4">
+      </video>
+      <img class="video-parallax image-desktop" src="/img/homepage.jpg" alt=""/>
       <div id="logo-container" class="logo-container">
         <img class="logo" src="/img/refurniture.svg" alt="" width="300" />
-        <h1 class="title">We make new<br>begginings <span id="keyword" class="keyword">easier.<span><span id="cursor"></span></h1>
+        <h1 class="title">We make new<br>beginnings <span id="keyword" class="keyword">easier.<span><span id="cursor"></span></h1>
         <div class="link-container">
             <a class="bi bi-arrow-right-circle-fill link-to-join" href={{url('/join')}}></a>
         </div>
