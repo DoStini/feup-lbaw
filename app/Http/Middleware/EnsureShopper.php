0cc0bf00f1d89d58e65503c3783b6b96b59b6547
Nuno Alves <up201908250@fe.up.pt>
2021-12-30 20:12:46 +0000

    change: removed sanctum successfully

--- app/Http/Middleware/EnsureShopper.php
+++ app/Http/Middleware/EnsureShopper.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Support\Facades\Auth;
 
-class MustBeShopper {
+class EnsureShopper {
     /**
      * Handle an incoming request.
      *
