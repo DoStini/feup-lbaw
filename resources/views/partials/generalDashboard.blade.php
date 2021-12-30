fabaad4ae5451c6df30ab6a67c81a3f77532a7c4
biromiro <up201906272@up.pt>
2021-12-30 12:44:55 +0000

    Add main dashboard cards

---
+++ resources/views/partials/generalDashboard.blade.php
@@ -0,0 +1,42 @@
+<div class="container h-100">
+    <div class="row-sm-5 d-flex h-50 justify-content-around align-items-center">
+        <div class="col-sm-5 h-75">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5 h-75">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row-sm-5 d-flex h-50 justify-content-around align-items-center">
+        <div class="col-sm-5 h-75">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5 h-75">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
