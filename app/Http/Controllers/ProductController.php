c9c47f0804e6adb9c5f983094004f22e925750af
biromiro <up201906272@up.pt>
2022-01-01 19:35:36 +0000

    Remove debugging dump and die

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -98,7 +98,6 @@ class ProductController extends Controller {
                 "query" => $query->get()
             ]);
         } catch (Exception $e) {
-            dd($e);
             return response()->json(
                 ['message' => 'Unexpected error'],
                 401
