8f5a8c4afa7676185eba376e9c3d814e4475c6fd
biromiro <up201906272@up.pt>
2022-01-17 14:13:20 +0000

    Fix filters to show on top

--- resources/views/pages/search/products.blade.php
+++ resources/views/pages/search/products.blade.php
@@ -7,10 +7,10 @@
 <form id="search-form" class="h-100">
     <div class="container h-100 w-100 mw-100 ps-5 pe-5 mt-4 mb-4">
         <div class="col">
+            <div class="row container mw-100 h-100 mb-4">
+                @include('partials.search.productfilters')
+            </div>
             <div class="row container mw-100 h-100">
-                <div class="col-md-3 col-xs-12">
-                    @include('partials.search.productfilters')
-                </div>
                 <div id="search-area" class="col-md-9 col-xs-12">
                     <div class="container w-100 pe-0">
                         <div id="products-area" class="row justify-content-start">
