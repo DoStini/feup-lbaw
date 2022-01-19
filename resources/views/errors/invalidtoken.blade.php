eb39d6fb70b6edb704764d6d79ba609cbb13f9fa
biromiro <up201906272@up.pt>
2022-01-19 13:20:50 +0000

    Add error blades

--- resources/views/errors/invalidtoken.blade.php
+++ resources/views/errors/invalidtoken.blade.php
@@ -1,15 +1,18 @@
-@extends('layouts.logoOnlyApp')
+@extends('layouts.error')
 
 @section('content')
 
-<section class="container">
-    <h2>Invalid Link</h2>
-    <p>The password recover link has expired or is invalid</p>
-    <p>
-        <a href="{{route('join')}}">Login</a>
-    </p>
-    <p>
-        <a href="{{route('recoverPage')}}">Recover</a>
-    </p>
+<section class="page page-404 vw-100 vh-100">
+    <div class="information-text information-text-404">
+        <a href={{route('getProductSearch')}} class="ms-md-2 w-100 d-flex justify-content-center">
+            <img src="/img/refurniture.svg" alt="" width="350" height="120" />
+          </a>
+        <h2 class="text-center message-title">Invalid Link</h2>
+        <p class="text-center message-error">The password recovery link has expired or is invalid.</p>
+        <div class="d-flex w-100 justify-content-evenly">
+            <a class="btn btn-primary w-25" href="{{route('join')}}">Login</a>
+            <a class="btn btn-primary w-25" href="{{route('recoverPage')}}">Recover</a>
+        </div>
+    </div>
 </section>
 @endsection
