383ebeb187d5214fd205822e32e03be4967591ea
biromiro <up201906272@up.pt>
2022-01-20 12:39:24 +0000

    Fix category behaviour

--- resources/views/partials/search/category.blade.php
+++ resources/views/partials/search/category.blade.php
@@ -1,4 +1,4 @@
-<li id="category-{{$category->id}}" class="list-group-item @if(count($category->child_categories) > 0) dropend @endif">
+<li id="category-{{$category->id}}" class="list-group-item cat-item @if(count($category->child_categories) > 0) dropend @endif">
     <input group="category-input" name={{$category->id}} id="category-{{$category->id}}" class="form-check-input" type="checkbox"
     value="category-active">
     <a class="text-decoration-none text-black"> {{$category->name}}</a>
@@ -16,6 +16,10 @@
  
 <script>
     let catIsOpen{{$category->id}} = false;
+    $('#category-{{$category->id}}').on('click', function(e) {
+        e.stopPropagation();
+    });
+    
     $('#category-{{$category->id}}.dropend').on('click', function(e){
         e.stopPropagation();
         $('.category-dropdown-{{$level}}').dropdown('hide');
