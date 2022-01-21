49ede3eefaa3865820b44034d59ee4c2d509eb8f
Nuno Alves <up201908250@fe.up.pt>
2022-01-21 15:08:04 +0000

    fix: about us centering

--- resources/views/pages/staticPages/aboutUs.blade.php
+++ resources/views/pages/staticPages/aboutUs.blade.php
@@ -7,13 +7,13 @@
         <h1 class="text-center mb-4">Our Purpose</h1>
     </div>
     <div class="row">
-      <div class="col-md-1"></div>
+      <div class="col-md"></div>
       <div class="col-md-4">
         <h3 class="text-start">The <em>Why</em></h3>
-        <p class="text-start">reFurniture is an eco-friendly online store for second-handed refurbished furniture. 
+        <p class="text-start">reFurniture is an eco-friendly online store for second-handed refurbished furniture.
           Furniture is usually seen as a <em>use only once</em> type of buy. As such, a lot of <strong>good</strong>
           and <strong>beautiful</strong> furnishings get thrown out, when they could easily be refurbished and sold
-          and yet still maintaining its initial purpose and value. As such, we seek to be the link that has been 
+          and yet still maintaining its initial purpose and value. As such, we seek to be the link that has been
           missing in this type of business, by providing our customers with memorable and yet affordable new items
           for their household.</p>
 
@@ -22,10 +22,10 @@
       <div class="col-md-4">
         <img class="" src="/img/aboutus1.jpg" style='height: 100%; width: 100%; object-fit: contain' alt="Eco-Friendly">
       </div>
-      <div class="col-md-1"></div>
+      <div class="col-md"></div>
     </div>
     <div class="row my-3">
-      <div class="col-md-1"></div>
+      <div class="col-md"></div>
       <div class="col-md-4">
         <img class="" src="/img/aboutus2.jpg" style='height: 100%; width: 100%; object-fit: contain' alt="Working Team">
 
@@ -37,20 +37,20 @@
         <p class="text-start">Firstly, we've looked up the market to find how we can achieve our goal. After defining
           specific places where we could consistently find worn out but recoverable furniture, we only needed to assemble
           a driven and hard-working team, and a place to do all the messy work you do not need to see! As such, our HQ are
-          based in Paços de Ferreira - who would've guessed that the <strong>furniture</strong> store would choose the 
+          based in Paços de Ferreira - who would've guessed that the <strong>furniture</strong> store would choose the
           <strong>furniture capital</strong> - and, ever since 2020, our process has been running smoothly. Our team is constantly
           growing, so don't miss out on possible career offers - <em>keep an eye</em> on our social media!
         </p>
       </div>
-      <div class="col-md-1"></div>
+      <div class="col-md"></div>
     </div>
     <div class="row my-2"></div>
   <div class="row">
     <h1 class="text-center my-5">Our Team</h1>
   </div>
   <div class="row my-4"></div>
-    <div class="row">
-        <div class="col-md-3 col-sm-6 col-12 my-2">
+    <div class="row about-us-photos">
+        <div class="col-lg-3 col-sm-6 col-12 my-2">
             <div class="card">
               <img class="card-img-top card-andre" src="" alt=" ">
               <div class="card-body text-center">
@@ -63,7 +63,7 @@
               </div>
             </div>
         </div>
-        <div class="col-md-3 col-sm-6 col-12 my-2">
+        <div class="col-lg-3 col-sm-6 col-12 my-2">
           <div class="card">
             <img class="card-img-top card-joao" src="" alt=" ">
             <div class="card-body text-center">
@@ -76,7 +76,7 @@
             </div>
           </div>
         </div>
-        <div class="col-md-3 col-sm-6 col-12 my-2">
+        <div class="col-lg-3 col-sm-6 col-12 my-2">
           <div class="card">
             <img class="card-img-top card-nunoa" src="" alt=" ">
             <div class="card-body text-center">
@@ -89,7 +89,7 @@
             </div>
           </div>
         </div>
-        <div class="col-md-3 col-sm-6 col-12 my-2">
+        <div class="col-lg-3 col-sm-6 col-12 my-2">
           <div class="card">
             <img class="card-img-top card-nunoc" src="" alt=" ">
             <div class="card-body text-center">
